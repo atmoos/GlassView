fe62e68715c1223114ad5129b274c5d695a2625b
Thomas Kägi <kaegit@gmail.com>
2024-05-10 20:54:00 +0200

    Optimise directory export impl.

--- source/GlassView.Export/DirectoryExport.cs
+++ source/GlassView.Export/DirectoryExport.cs
@@ -1,4 +1,6 @@
+using System.Text.Json;
 using BenchmarkDotNet.Reports;
+using GlassView.Core;
 using GlassView.Core.Models;
 
 using static GlassView.Export.Mapping;
@@ -7,12 +9,16 @@ namespace GlassView.Export;
 
 internal sealed class DirectoryExport(DirectoryInfo path) : IExport
 {
-    public async Task Export(Summary summary, CancellationToken token)
+    private static readonly JsonSerializerOptions options = new JsonSerializerOptions().EnableGlassView();
+    public async Task Export(Summary inputSummary, CancellationToken token)
     {
-        BenchmarkSummary values = Map(summary);
-        String json = values.Serialize();
-        String filePath = Path.Combine(path.FullName, $"{values.Name}.json");
-        await File.WriteAllTextAsync(filePath, json, token).ConfigureAwait(false);
+        var summary = Map(inputSummary);
+        FileInfo file = CombineToFile(path, summary);
+        using var stream = file.Open(FileMode.Create, FileAccess.Write, FileShare.None);
+        await JsonSerializer.SerializeAsync(stream, summary, options, token).ConfigureAwait(ConfigureAwaitOptions.None);
     }
     public override String ToString() => $"{nameof(Export)}: {path.FullName}";
+
+    private static FileInfo CombineToFile(DirectoryInfo path, BenchmarkSummary summary)
+        => new(Path.Combine(path.FullName, $"{summary.Name}.json"));
 }
