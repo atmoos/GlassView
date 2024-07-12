6fa6f119b4db0b76495960f0d0878a68eff43c18
Thomas Kägi <kaegit@gmail.com>
2024-07-12 21:52:31 +0200

    Assert exported json content.

--- source/GlassView.Export.Test/DirectoryExportTest.cs
+++ source/GlassView.Export.Test/DirectoryExportTest.cs
@@ -3,25 +3,38 @@ using BenchmarkDotNet.Loggers;
 using Atmoos.World;
 using Atmoos.GlassView.Core;
 using Atmoos.GlassView.Core.Models;
+using Xunit.Abstractions;
 
 namespace Atmoos.GlassView.Export.Test;
 
-public class DirectoryExportTest
+public class DirectoryExportTest(ITestOutputHelper output)
 {
+    private readonly ILogger logger = new TestLogger(output);
+    private static readonly JsonSerializerOptions options = new JsonSerializerOptions().EnableGlassView();
     private static readonly System.IO.FileInfo testFile = new(System.IO.Path.Combine("Resources", "TestBenchmark.json"));
 
     [Fact]
     public async Task ExportWritesJsonFile()
     {
-        var options = new JsonSerializerOptions().EnableGlassView();
         var summary = testFile.Deserialize<BenchmarkSummary>(options);
-        var emptyDirectory = CreateUniqueDirectory();
-        var export = new DirectoryExport<TestFileSystem>(emptyDirectory, options);
-        await export.Export(summary, NullLogger.Instance, CancellationToken.None);
 
-        var exportedFile = Assert.Single(emptyDirectory);
+        var exportedFile = await RunExportTest(summary);
+
         Assert.StartsWith(summary.Name, exportedFile.Name);
-        Assert.EndsWith(".json", exportedFile.Name);
+        Assert.Equal("json", exportedFile.Name.Extension);
+    }
+
+    [Fact]
+    public async Task ExportWritesExpectedJsonFile()
+    {
+        var summary = testFile.Deserialize<BenchmarkSummary>(options);
+
+        var exportedFile = await RunExportTest(summary);
+
+        using var actualText = exportedFile.OpenText();
+        using var expectedText = testFile.OpenText();
+        var expected = expectedText.ReadToEnd().ReplaceLineEndings(String.Empty);
+        Assert.Equal(expected, actualText.ReadToEnd(), ignoreAllWhiteSpace: true);
     }
 
     [Fact]
@@ -34,7 +47,6 @@ public class DirectoryExportTest
 
         var result = export.ToString();
 
-        // Assert
         Assert.All(pathComponents, component => Assert.Contains(component, result));
     }
 
@@ -42,5 +54,13 @@ public class DirectoryExportTest
     {
         var uniqueName = Guid.NewGuid().ToString();
         return TestFileSystem.Create(TestFileSystem.Root, new DirectoryName(uniqueName));
+    }
+
+    private async Task<IFile> RunExportTest(BenchmarkSummary summary)
+    {
+        var directory = CreateUniqueDirectory();
+        var export = new DirectoryExport<TestFileSystem>(directory, options);
+        await export.Export(summary, this.logger, CancellationToken.None).ConfigureAwait(false);
+        return Assert.Single(directory);
     }
 }
